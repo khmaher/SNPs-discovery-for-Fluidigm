95ca1a279e644904846fa15c478f45262588a205
khmaher <katymaheris@gmail.com>
2025-10-27 11:20:02 +0000

    Update 04_trimmomatic.sh

--- scripts/04_trimmomatic.sh
+++ scripts/04_trimmomatic.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH -A molecolb
-#SBATCH -p molecolb
+##SBATCH -A molecolb
+##SBATCH -p molecolb
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --time=72:00:00
 
